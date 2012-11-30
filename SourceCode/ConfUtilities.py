ad83aff5a614af43479c3412825ddbd4ca22f1b8
narfolik <narfolik@gmail.com>
2012-11-30 02:33:35 -0700

    Dodao modul za rad s konfiguracijama

---
+++ SourceCode/ConfUtilities.py
@@ -0,0 +1,42 @@
+#
+#   This module provides utilities for working with configuration
+#   FERnfs application.
+#
+
+import sys
+
+#
+#   Reads a configuration file and returns a dictionary with
+#   values mapped to their attribute names or None if file is 
+#   not valid or could not be read.
+#@param fileName
+#@return
+#
+def readConfigFile(fileName):
+    params = {}
+    try:
+        for line in open(fileName, "r"):
+            line = line.strip()
+            if line.startswith("#"):
+                continue
+            parts = line.split( "=", 1)
+            params[parts[0].strip()] = parts[1].strip()
+    except IOError as e:
+        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+        return None
+    except IndexError:
+        print "Invalid configuration file."
+        return None
+
+    return params
+
+#
+#   Checks if given configuration can be used to run the aplication.
+#@param config configuration dictionary as produced by readConfigFile
+#@return true if configuration is valid
+def checkConfiguration(config):
+    # TODO once configuration attributes are determined
+    return 1
+
+if __name__ == "__main__":
+    print readConfigFile(sys.argv[1])
